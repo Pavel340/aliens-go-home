996846165f2b0f710c1e80243b0a904555035bd8
Pavel340 <zinovev-pasha@mail.ru>
2018-04-24 09:30:34 +0300

    Add files via upload

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,45 @@
-import { MOVE_OBJECTS } from '../actions';
+import {
+  LEADERBOARD_LOADED, LOGGED_IN,
+  MOVE_OBJECTS, SHOOT, START_GAME
+} from '../actions';
 import moveObjects from './moveObjects';
+import startGame from './startGame';
+import shoot from './shoot';
 
-const initailState = {
-  angle: 45,
+const initialGameState = {
+  started: false,
+  kills: 0,
+  lives: 3,
+  flyingObjects: [],
+  lastObjectCreatedAt: new Date(),
+  currentPlayer: null,
+  players: null,
+  cannonBalls: [],
 };
 
-function reducer(state = initailState, action) {
-  switch( action.type ) {
+const initialState = {
+  angle: 45,
+  gameState: initialGameState,
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case LEADERBOARD_LOADED:
+      return {
+        ...state,
+        players: action.players,
+      };
+    case LOGGED_IN:
+      return {
+        ...state,
+        currentPlayer: action.player,
+      };
     case MOVE_OBJECTS:
-      return moveObjects( state, action );
+      return moveObjects(state, action);
+    case START_GAME:
+      return startGame(state, initialGameState);
+    case SHOOT:
+      return shoot(state, action);
     default:
       return state;
   }
